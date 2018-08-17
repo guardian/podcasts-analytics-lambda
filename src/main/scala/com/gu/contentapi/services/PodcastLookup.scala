120373eb3f211faafa29f1dc21b8079c848b7f30
tomrf1 <tom.forbes@theguardian.com>
2018-08-17 17:08:16 +0100

    Retry capi requests

--- src/main/scala/com/gu/contentapi/services/PodcastLookup.scala
+++ src/main/scala/com/gu/contentapi/services/PodcastLookup.scala
@@ -1,15 +1,17 @@
 package com.gu.contentapi.services
 
 import com.gu.contentapi.client._
-import com.gu.contentapi.client.model.v1.{ SearchResponse, Tag }
-import com.gu.contentapi.client.model.{ ContentApiError, SearchQuery }
+import com.gu.contentapi.client.model.v1.SearchResponse
+import com.gu.contentapi.client.model.{ContentApiError, SearchQuery}
 
 import scala.concurrent.duration._
-import scala.concurrent.{ Await, Future }
+import scala.concurrent.{Await, Future}
 import com.gu.contentapi.Config.capiKey
 import org.apache.logging.log4j.scala.Logging
 
 import scala.collection.concurrent.TrieMap
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 object PodcastLookup extends Logging {
 
@@ -52,9 +54,21 @@ object PodcastLookup extends Logging {
       cache.get(filePath)
     } else {
       cacheMisses += 1
+      tryQuery(filePath) match {
+        case Success(info) => info
+        case Failure(NonFatal(e)) =>
+          logger.error(s"CAPI request repeatedly failed: ${e.getMessage}", e)
+          None
+      }
+    }
+  }
+
+  //Try up to 3 times because we occasionally get capi timeouts
+  private def tryQuery(filePath: String, retries: Int = 3): Try[Option[PodcastInfo]] = {
+    Try {
       Await.result(
         makeCapiQuery(filePath) map {
-          case SuccessfulQuery(sr) => {
+          case SuccessfulQuery(sr) =>
             for {
               result <- sr.results.headOption
               podcastName <- result.tags.find(_.podcast.isDefined).map(_.webUrl)
@@ -63,12 +77,15 @@ object PodcastLookup extends Logging {
               cache.put(filePath, podcastInfo)
               podcastInfo
             }
-          }
+
           case FailedQuery(err) =>
             logger.error(s"Failed to get podcast info from capi for file '$filePath': $err")
             None
-
         }, 5.seconds)
+    }.recoverWith {
+      case NonFatal(e) if retries > 0 =>
+        logger.info(s"CAPI request failed with ${retries-1} retries to go: ${e.getMessage}", e)
+        tryQuery(filePath, retries - 1)
     }
   }
 }
