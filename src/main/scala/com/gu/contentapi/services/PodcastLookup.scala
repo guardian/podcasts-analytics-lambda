caa2c9cb0bee4ba8c4731d687b8d0cebb581852f
tomrf1 <tom.forbes@theguardian.com>
2018-08-20 10:14:24 +0100

    nicer

--- src/main/scala/com/gu/contentapi/services/PodcastLookup.scala
+++ src/main/scala/com/gu/contentapi/services/PodcastLookup.scala
@@ -2,16 +2,16 @@ package com.gu.contentapi.services
 
 import com.gu.contentapi.client._
 import com.gu.contentapi.client.model.v1.SearchResponse
-import com.gu.contentapi.client.model.{ContentApiError, SearchQuery}
+import com.gu.contentapi.client.model.{ ContentApiError, SearchQuery }
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{ Await, Future }
 import com.gu.contentapi.Config.capiKey
 import org.apache.logging.log4j.scala.Logging
 
 import scala.collection.concurrent.TrieMap
 import scala.util.control.NonFatal
-import scala.util.{Failure, Success, Try}
+import scala.util.{ Failure, Success, Try }
 
 object PodcastLookup extends Logging {
 
@@ -54,7 +54,28 @@ object PodcastLookup extends Logging {
       cache.get(filePath)
     } else {
       cacheMisses += 1
-      tryQuery(filePath) match {
+
+      //Try up to 3 times because we occasionally get capi timeouts
+      val result: Try[Option[PodcastLookup.PodcastInfo]] = retry(3) {
+        Await.result(
+          makeCapiQuery(filePath) map {
+            case SuccessfulQuery(sr) =>
+              for {
+                result <- sr.results.headOption
+                podcastName <- result.tags.find(_.podcast.isDefined).map(_.webUrl)
+              } yield {
+                val podcastInfo = PodcastInfo(result.webUrl, podcastName)
+                cache.put(filePath, podcastInfo)
+                podcastInfo
+              }
+
+            case FailedQuery(err) =>
+              logger.error(s"Failed to get podcast info from capi for file '$filePath': $err")
+              None
+          }, 5.seconds)
+      }
+
+      result match {
         case Success(info) => info
         case Failure(NonFatal(e)) =>
           logger.error(s"CAPI request repeatedly failed: ${e.getMessage}", e)
@@ -63,29 +84,11 @@ object PodcastLookup extends Logging {
     }
   }
 
-  //Try up to 3 times because we occasionally get capi timeouts
-  private def tryQuery(filePath: String, retries: Int = 3): Try[Option[PodcastInfo]] = {
-    Try {
-      Await.result(
-        makeCapiQuery(filePath) map {
-          case SuccessfulQuery(sr) =>
-            for {
-              result <- sr.results.headOption
-              podcastName <- result.tags.find(_.podcast.isDefined).map(_.webUrl)
-            } yield {
-              val podcastInfo = PodcastInfo(result.webUrl, podcastName)
-              cache.put(filePath, podcastInfo)
-              podcastInfo
-            }
-
-          case FailedQuery(err) =>
-            logger.error(s"Failed to get podcast info from capi for file '$filePath': $err")
-            None
-        }, 5.seconds)
-    }.recoverWith {
-      case NonFatal(e) if retries > 0 =>
-        logger.info(s"CAPI request failed with ${retries-1} retries to go: ${e.getMessage}", e)
-        tryQuery(filePath, retries - 1)
+  private def retry[T](retries: Int)(f: => T): Try[T] = {
+    Try(f).recoverWith {
+      case NonFatal(e) if retries > 1 =>
+        logger.info(s"CAPI request failed with ${retries - 1} retries to go: ${e.getMessage}", e)
+        retry(retries - 1)(f)
     }
   }
 }
