5bc40af2b4be0bd6a2032108393ba31ff8d77fe3
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2019-02-22 13:13:55 +0000

    Avoid processing the list of records twice, DRY the code

--- src/main/scala/com/gu/contentapi/Lambda.scala
+++ src/main/scala/com/gu/contentapi/Lambda.scala
@@ -18,57 +18,40 @@ class Lambda extends RequestHandler[S3Event, Unit] with Logging {
 
   override def handleRequest(event: S3Event, context: Context): Unit = {
 
-    val fastlyReportObjects = event.getRecords.asScala
-      .filter(rec => isLogType(Config.FastlyAudioLogsBucketName, rec.getS3))
-      .flatMap(rec => S3.downloadReport(rec.getS3.getBucket.getName, rec.getS3.getObject.getKey.replaceAll("%3A", ":"))) // looks like the json object is not decoded properly by the SKD
-      .toList
+    val (fastlyRecords, maybeAcastRecords) = event.getRecords.asScala
+      .partition(rec => isLogType(Config.FastlyAudioLogsBucketName, rec.getS3))
+    val (acastRecords, otherRecords) = maybeAcastRecords
+      .partition(rec => isLogType(Config.AcastAudioLogsBucketName, rec.getS3))
 
-    val acastReportObjects = event.getRecords.asScala
-      .filter(rec => isLogType(Config.AcastAudioLogsBucketName, rec.getS3))
-      .flatMap(rec => S3.downloadReport(rec.getS3.getBucket.getName, rec.getS3.getObject.getKey.replaceAll("%3A", ":"))) // looks like the json object is not decoded properly by the SKD
-      .toList
-
-    handleReportsFromFastly(fastlyReportObjects)
-
-    handleReportsFromAcast(acastReportObjects)
+    process[FastlyLog](r => FastlyLog.onlyDownloads(r) && FastlyLog.onlyGet(r), Event(_), FastlyLog(_))(fastlyRecords)
+    process[AcastLog](r => AcastLog.onlyDownloads(r) && AcastLog.onlySuccessfulReponses(r) && AcastLog.onlyGet(r), Event(_), AcastLog(_))(acastRecords)
 
     logger.debug(s"Cache hits: ${PodcastLookup.cacheHits}")
     logger.debug(s"Cache misses: ${PodcastLookup.cacheMisses}")
-
   }
+
+  private final def process[A](
+    predicate: A => Boolean,
+    toEvent: A => collection.GenTraversableOnce[Event],
+    builder: String => collection.GenTraversableOnce[A])(objects: Buffer[S3EventNotificationRecord]): Unit =
+    objects.toList
+      .flatMap(rec => S3.downloadReport(rec.getS3.getBucket.getName, rec.getS3.getObject.getKey.replaceAll("%3A", ":"))) // looks like the json object is not decoded properly by the SKD
+      .map(toDownloadLogs[A](predicate, builder))
+      .foreach(downloadsLogs => Ophan.send(downloadsLogs.flatMap(toEvent)))
 
   private def isLogType(typeName: String, s3Entity: S3Entity): Boolean =
     s3Entity.getBucket.getName == typeName ||
       s3Entity.getObject.getKey.startsWith(typeName)
 
-  private def handleReportsFromFastly(objects: List[S3Object]): Unit = {
-    objects foreach { obj =>
+  private def toDownloadLogs[B](predicate: B => Boolean, builder: String => collection.GenTraversableOnce[B])(obj: S3Object): List[B] = {
+    import com.gu.contentapi.utils.PredicateUtils._
 
-      val allFastlyLogs = Source.fromBytes(IOUtils.toByteArray(obj.getObjectContent))("ISO-8859-1").getLines().toSeq flatMap { line =>
-        FastlyLog(line)
-      }
-
-      import com.gu.contentapi.utils.PredicateUtils._
-
-      val downloadsLogs = allFastlyLogs.filter(FastlyLog.onlyDownloads && FastlyLog.onlyGet)
-
-      Ophan.send(downloadsLogs.flatMap(Event(_)))
-    }
-  }
-
-  private def handleReportsFromAcast(objects: List[S3Object]): Unit = {
-    objects foreach { obj =>
-
-      val allAcastLogs = Source.fromBytes(IOUtils.toByteArray(obj.getObjectContent))("ISO-8859-1").getLines().toSeq flatMap { line =>
-        AcastLog(line)
-      }
-
-      import com.gu.contentapi.utils.PredicateUtils._
-
-      val downloadsLogs = allAcastLogs.filter(AcastLog.onlyDownloads && AcastLog.onlySuccessfulReponses && AcastLog.onlyGet)
-
-      Ophan.send(downloadsLogs.flatMap(Event(_)))
-    }
+    Source
+      .fromBytes(IOUtils.toByteArray(obj.getObjectContent))("ISO-8859-1")
+      .getLines
+      .toList
+      .flatMap(builder)
+      .filter(predicate)
   }
 
 }
