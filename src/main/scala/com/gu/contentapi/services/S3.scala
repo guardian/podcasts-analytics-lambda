2d05a42ab7d4f1dec7b3e9cc7d6adec69ddec5e0
Paul Roberts <paul.roberts@guardian.co.uk>
2023-03-07 17:27:26 +0000

    retry the downloading of the report from S3
    when it fails

--- src/main/scala/com/gu/contentapi/services/S3.scala
+++ src/main/scala/com/gu/contentapi/services/S3.scala
@@ -9,17 +9,25 @@ import scala.util.{ Failure, Success, Try }
 
 object S3 extends Logging {
 
+  private val retryDelayMs = 100
+
   val client: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build()
 
-  def downloadReport(bucketName: String, reportName: String): Option[S3Object] = {
+  def downloadReport(bucketName: String, reportName: String, retries: Int = 3): Option[S3Object] = {
     Try(client.getObject(bucketName, reportName)) match {
       case Success(report) => {
         logger.info(s"Downloaded report $reportName")
         Some(report)
       }
       case Failure(e) => {
-        logger.warn(s"Failed to download $reportName - exception thrown $e")
-        None
+        if (retries > 0) {
+          logger.info(s"Failed to download $reportName - remaining retries: $retries - exception thrown $e")
+          Thread.sleep(retryDelayMs)
+          downloadReport(bucketName, reportName, retries - 1)
+        } else {
+          logger.warn(s"Failed to download $reportName - no retries left - exception thrown $e")
+          None
+        }
       }
     }
   }
